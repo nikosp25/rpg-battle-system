9a118d9c37cf503cbac2d8936684420d61ef75f5
Nikos.p <connor2000@hotmail.gr>
2026-01-10 01:30:56 +0200

    feat: add attack method in Knight class.

--- src/com/game/rpg/entities/classes/Knight.java
+++ src/com/game/rpg/entities/classes/Knight.java
@@ -9,4 +9,25 @@ public class Knight extends Entity {
         super(name,new Attributes(200.0,200,100),
                 new CombatStats(50.0,0.15,1.4,0.03,0.4,12,45.0,10.0,20.0));
     }
+
+
+    /**
+     * Uses the decapitation skill,
+     * costs 20 stamina.
+     * Deals damage based of the physical dmg stat.
+     * @param target the entity to attack.
+     */
+
+    @Override
+    public void attack(Entity target) {
+        int cost = 20;
+        if (!spendStamina(cost)){
+            return;
+        }
+
+        double decapitation = calculateAttack(false);
+
+        System.out.println(getName() + " uses decapitation on " + target.getName() + "!");
+        target.takeDamage(decapitation,false);
+    }
 }
