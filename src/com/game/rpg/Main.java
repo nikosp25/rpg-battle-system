d9678a45c35826a6efcf0b4fb651caca393228a3
Nikos.p <connor2000@hotmail.gr>
2026-01-10 01:48:56 +0200

    feat:add UI and battle systems in Main
    Wired the intro narration and character selection to the BattleManager.
    
    Established the core game loop from startup to the final dragon encounter.

--- src/com/game/rpg/Main.java
+++ src/com/game/rpg/Main.java
@@ -1,12 +1,127 @@
 package com.game.rpg;
 
-import com.game.rpg.components.Attributes;
-import com.game.rpg.components.CombatStats;
+import com.game.rpg.battleLogic.BattleManager;
 import com.game.rpg.entities.Entity;
+import com.game.rpg.entities.classes.Knight;
+import com.game.rpg.entities.classes.Mage;
+import com.game.rpg.entities.classes.Rogue;
+import com.game.rpg.ui.ClassSelectIntro;
+import com.game.rpg.ui.Intro;
+import com.game.rpg.ui.Narrator;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-    }
+        Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Entity> party = new ArrayList<>();
+
+        try {
+            Intro.play();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 2 second delay before class selection.
+        try {
+            Thread.sleep(2000); //
+        } catch (InterruptedException e) {
+            //ignore
+        }
+
+
+        ClassSelectIntro.printClass();
+        ClassSelectIntro.showStats();
+
+
+        int choice = 0;
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+
+                if (choice >= 1 && choice <= 3) {
+                    break;
+                } else {
+                    System.out.println("""
+                            ╔════════════════════════════════════════╗
+                            ║                                        ║
+                            ║   ⚠️  Please enter a correct number   ║
+                            ║           between 1 - 3                ║
+                            ║                                        ║
+                            ╚════════════════════════════════════════╝
+                            """);
+
+                }
+
+
+            } else {
+                System.out.println("That is not a number!");
+                scanner.next();
+            }
+        }
+        scanner.nextLine(); //clears scanner from earlier uses. Avoids scanner glitch
+        ClassSelectIntro.chooseName();
+        String charName = scanner.nextLine();
+
+        //Adds the player class choice first in the party list, so they can attack first,
+        //based on the Battle logic.
+
+        switch (choice) {
+            case 1:
+                System.out.println("You choose Knight!");
+
+                party.add(new Knight(charName));
+
+                //add the 2 remaining classes with generic predetermined names.
+                party.add(new Rogue("Shadow"));
+                party.add(new Mage("Inferius"));
+                break;
+
+            case 2:
+                System.out.println("You choose Mage!");
+
+                party.add(new Mage(charName));
+
+
+                party.add(new Rogue("Shadow"));
+                party.add(new Knight("Artorious"));
+                break;
+
+            case 3:
+                System.out.println("You choose Rogue!");
+
+                party.add(new Rogue(charName));
+
+
+                party.add(new Knight("Artorious"));
+                party.add(new Mage("Inferius"));
+                break;
+
+
+        }
+
+        // small delay between character select and game starting.
+        try {
+            Thread.sleep(2000); //
+        } catch (InterruptedException e) {
+            //ignore
+        }
+        Narrator.dungeonEntry();
+
+        BattleManager battleManager = new BattleManager(); //start the game logic.
+
+        battleManager.startDungeon(party); //Pass the created party into the game logic so the game can begin.
+
+        scanner.close();
+
+
+
+
+
+    }
 }
+
