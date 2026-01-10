ca204802e17f4e3ce517e9927343ccb65bf295b9
Nikos.p <connor2000@hotmail.gr>
2026-01-10 15:46:14 +0200

    feat: add loop so the user can play again if they choose to after clearing the dragon encounter.

--- src/com/game/rpg/Main.java
+++ src/com/game/rpg/Main.java
@@ -10,112 +10,129 @@ import com.game.rpg.ui.Intro;
 import com.game.rpg.ui.Narrator;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        ArrayList<Entity> party = new ArrayList<>();
-
-        try {
-            Intro.play();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // 2 second delay before class selection.
-        try {
-            Thread.sleep(2000); //
-        } catch (InterruptedException e) {
-            //ignore
-        }
+        boolean playAgain = true;
+        do {
 
 
-        ClassSelectIntro.printClass();
-        ClassSelectIntro.showStats();
+            ArrayList<Entity> party = new ArrayList<>();
 
-
-        int choice = 0;
-
-        while (true) {
-            if (scanner.hasNextInt()) {
-                choice = scanner.nextInt();
-
-                if (choice >= 1 && choice <= 3) {
-                    break;
-                } else {
-                    System.out.println("""
-                            ╔════════════════════════════════════════╗
-                            ║                                        ║
-                            ║   ⚠️  Please enter a correct number   ║
-                            ║           between 1 - 3                ║
-                            ║                                        ║
-                            ╚════════════════════════════════════════╝
-                            """);
-
-                }
-
-
-            } else {
-                System.out.println("That is not a number!");
-                scanner.next();
+            try {
+                Intro.play();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }
-        scanner.nextLine(); //clears scanner from earlier uses. Avoids scanner glitch
-        ClassSelectIntro.chooseName();
-        String charName = scanner.nextLine();
 
-        //Adds the player class choice first in the party list, so they can attack first,
-        //based on the Battle logic.
-
-        switch (choice) {
-            case 1:
-                System.out.println("You choose Knight!");
-
-                party.add(new Knight(charName));
-
-                //add the 2 remaining classes with generic predetermined names.
-                party.add(new Rogue("Shadow"));
-                party.add(new Mage("Inferius"));
-                break;
-
-            case 2:
-                System.out.println("You choose Mage!");
-
-                party.add(new Mage(charName));
+            // 2 second delay before class selection.
+            try {
+                Thread.sleep(2000); //
+            } catch (InterruptedException e) {
+                //ignore
+            }
 
 
-                party.add(new Rogue("Shadow"));
-                party.add(new Knight("Artorious"));
-                break;
-
-            case 3:
-                System.out.println("You choose Rogue!");
-
-                party.add(new Rogue(charName));
+            ClassSelectIntro.printClass();
+            ClassSelectIntro.showStats();
 
 
-                party.add(new Knight("Artorious"));
-                party.add(new Mage("Inferius"));
-                break;
+            int choice = 0;
+
+            while (true) {
+                if (scanner.hasNextInt()) {
+                    choice = scanner.nextInt();
+
+                    if (choice >= 1 && choice <= 3) {
+                        break;
+                    } else {
+                        System.out.println("""
+                                ╔════════════════════════════════════════╗
+                                ║                                        ║
+                                ║   ⚠️  Please enter a correct number   ║
+                                ║           between 1 - 3                ║
+                                ║                                        ║
+                                ╚════════════════════════════════════════╝
+                                """);
+
+                    }
 
 
-        }
+                } else {
+                    System.out.println("That is not a number!");
+                    scanner.next();
+                }
+            }
+            scanner.nextLine(); //clears scanner from earlier uses. Avoids scanner glitch
+            ClassSelectIntro.chooseName();
+            String charName = scanner.nextLine();
 
-        // small delay between character select and game starting.
-        try {
-            Thread.sleep(2000); //
-        } catch (InterruptedException e) {
-            //ignore
-        }
-        Narrator.dungeonEntry();
+            //Adds the player class choice first in the party list, so they can attack first,
+            //based on the Battle logic.
 
-        BattleManager battleManager = new BattleManager(); //start the game logic.
+            switch (choice) {
+                case 1:
+                    System.out.println("You choose Knight!");
 
-        battleManager.startDungeon(party); //Pass the created party into the game logic so the game can begin.
+                    party.add(new Knight(charName));
 
+                    //add the 2 remaining classes with generic predetermined names.
+                    party.add(new Rogue("Shadow"));
+                    party.add(new Mage("Inferius"));
+                    break;
+
+                case 2:
+                    System.out.println("You choose Mage!");
+
+                    party.add(new Mage(charName));
+
+
+                    party.add(new Rogue("Shadow"));
+                    party.add(new Knight("Artorious"));
+                    break;
+
+                case 3:
+                    System.out.println("You choose Rogue!");
+
+                    party.add(new Rogue(charName));
+
+
+                    party.add(new Knight("Artorious"));
+                    party.add(new Mage("Inferius"));
+                    break;
+
+
+            }
+
+            // small delay between character select and game starting.
+            try {
+                Thread.sleep(2000); //
+            } catch (InterruptedException e) {
+                //ignore
+            }
+            Narrator.dungeonEntry();
+
+            BattleManager battleManager = new BattleManager(); //start the game logic.
+
+            battleManager.startDungeon(party); //Pass the created party into the game logic so the game can begin.
+
+            //ask player if they want to play again
+            System.out.println("Would you like to play again? Type Yes/No : "); // assume the user will only enter a string.
+            String answer = scanner.next().toLowerCase().trim();
+
+            if (answer.equals("yes")) {
+                System.out.println("Restarting the game!");
+
+            }
+            else {
+                playAgain = false;
+                System.out.println("Thank you for clearing the dungeon!");
+            }
+        } while (playAgain);
         scanner.close();
 
 
